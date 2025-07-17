f2559d16a35401867985ba10c3b12fe71976dce4
Hanafi Solihu Olayinka <Mathsolay@gmail.com>
2025-07-17 16:57:25 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  favorites: [
-    {
-      id: String,
-      title: String,
-      poster_path: String
-    }
-  ]
-}, { timestamps: true });
+const favoriteSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  poster_path: String,
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    favorites: [favoriteSchema],
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model('User', userSchema);
